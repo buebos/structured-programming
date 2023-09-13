5b3bc9e2020494f353a895c52ccb5958738eb075
emi <hr.emii87@gmail.com>
2023-09-12 20:14:32 -0700

    Feature: Added run executable to call files from dist folder only by filename

--- Index.c
+++ Index.c
@@ -1,6 +1,98 @@
+#include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 
-int main() {
-    printf("Hello world from C!\n");
+char *concat(const char *s1, const char *s2) {
+    char *result = malloc(strlen(s1) + strlen(s2) + 1);  // +1 for the null-terminator
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
+int searchForExe(char *basePath, char *executable, char *args, int depth) {
+    const char SEPARATOR[] = "\\";
+    char path[1000];
+    char executables[1000];
+    struct dirent *dp;
+    DIR *dir = opendir(basePath);
+
+    if (!dir) {
+        return 1;
+    }
+
+    while ((dp = readdir(dir)) != NULL) {
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0 && dp->d_name[0] != *".") {
+            strcpy(path, basePath);
+            strcat(path, SEPARATOR);
+            strcat(path, dp->d_name);
+
+            struct stat statbuf;
+            stat(path, &statbuf);
+
+            if (strcmp(dp->d_name, executable) == 0) {
+                int status = system(concat(path, concat(" ", args)));
+
+                return status;
+            } else if (S_ISDIR(statbuf.st_mode)) {
+                searchForExe(path, executable, args, depth + 1);
+            }
+        }
+    }
+
+    closedir(dir);
+
+    return -1;
+}
+
+int main(int argc, char *argv[]) {
+    char path[100] = "dist";
+    char *executable = argv[1];
+    char *argv2;
+    int k = 0;
+    int totalLen = 0;
+
+    for (int i = 0; i < argc - 2; i++) {
+        totalLen += strlen(argv[i + 2]);
+    }
+    argv2 = malloc((totalLen + 1) * sizeof(char));
+    if (argv2 == NULL) {
+        printf("[ERROR]: Failed to allocate memory for argv2\n");
+        return 1;
+    }
+    for (int i = 0; i < argc - 2; i++) {
+        int argLen = strlen(argv[i + 2]);
+        for (int j = 0; j < argLen; j++) {
+            argv2[k] = argv[i + 2][j];
+            printf("argv[i + 2][j] = %c\n", argv[i + 2][j]);
+            printf("argv2 = %s\n", argv2);
+            k++;
+        }
+    }
+    argv2[k] = '\0';
+
+    if (argc < 2) {
+        printf("[ERROR]: Please provide the name of the executable\n");
+        return 1;
+    }
+
+    int status = searchForExe(path, executable, argv2, 0);
+
+    for (int i = 0; i < argc - 2; i++) {
+        free(argv2);
+    }
+    free(argv2);
+
+    if (status == -1) {
+        printf("\n[ERROR]: Executable not found on dist folder\n");
+        return 1;
+    }
+
+    if (status != 0) {
+        printf("\n[ERROR]: Your executable from dist folder returned with error code %d\n", status);
+        return status;
+    }
+
     return 0;
 }
--- src/WordSearch.c
+++ src/WordSearch.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int arrlen(const void **arr) {
-    return sizeof(arr);
+    return sizeof(arr) / sizeof(arr[0]);
 }
 int cstrlen(const char *arr) {
     int i = 0;
@@ -45,8 +45,8 @@ int csetstrsplit(const char *str, const char *separators, char splitted[100][100
     return j;
 }
 int cstrequal(const char *str1, const char *str2) {
-    int strlen1 = 0;
-    int strlen2 = 0;
+    int strlen1 = cstrlen(str1);
+    int strlen2 = cstrlen(str2);
 
     if (strlen1 != strlen2) {
         return 0;
@@ -60,55 +60,48 @@ int cstrequal(const char *str1, const char *str2) {
 
     return 1;
 }
-
-void assignString(char *toAssign[], char *assignment[]) {
+void cstrarrpush(char strarr[][100], char *str, int *strarrlen) {
     int i = 0;
-    int maxStringSize = 0;
-
-    while (toAssign[i] != "\0") {
-        maxStringSize++;
-        i++;
-        printf("%d", i);
+    for (i = 0; i < cstrlen(str); i++) {
+        strarr[*strarrlen][i] = str[i];
     }
-    i = 0;
-    while (assignment[i] != "\0") {
-        if (i > maxStringSize) {
-            maxStringSize++;
-        }
-        i++;
-    }
-
-    printf("%s", maxStringSize);
-
-    for (i = 0; i < maxStringSize; i++) {
-        toAssign[i] = assignment[i];
-    }
-
-    return;
-}
-
-void fillStringArray(char *stringArr[][100], int arrSize) {
-    for (int i = 0; i < arrSize; i++) {
-        stringArr[i][0] = "\0";
-    }
+    (*strarrlen)++;
 
     return;
 }
 
 int main(int argc, char *argv[]) {
-    const char TEXT[] = "Hello world\nfrom x";
-    const char SEPARATORS[] = " \n\t";
+    if (argc < 2) {
+        printf("[ERROR]: Please provide the text and at least one search word as arguments.\n");
+        return 1;
+    }
+
+    const char *TEXT = argv[1];
+    const char WORD_SEPARATORS[] = " \n\t.,;:";
     char searchWords[100][100];
     char foundWords[100][100];
     char textWords[100][100];
-    int wordsLen = csetstrsplit(TEXT, SEPARATORS, textWords);
-    int searchWordsLen = 5;
+    int wordsLen = csetstrsplit(TEXT, WORD_SEPARATORS, textWords);
+    int searchWordsLen = 0;
+    int foundWordsLen = 0;
+
+    for (int i = 2; i < argc; i++) {
+        cstrarrpush(searchWords, argv[i], &searchWordsLen);
+    }
 
     for (int i = 0; i < searchWordsLen; i++) {
         for (int j = 0; j < wordsLen; j++) {
             if (cstrequal(searchWords[i], textWords[j])) {
-                
+                cstrarrpush(foundWords, searchWords[i], &foundWordsLen);
             }
+        }
+    }
+
+    if (foundWordsLen == 0) {
+        printf("[INFO]: No matches found in text, be sure to use quotes for the text to make the search on\n[EXAMPLE]: WordSearch \"Search text here\" this are words that will be searched individually");
+    } else {
+        for (int i = 0; i < foundWordsLen; i++) {
+            printf("[INFO]: Found %d: %s\n", i + 1, foundWords[i]);
         }
     }
 
